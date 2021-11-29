fb2215172ab62b2505e3ee0613b61542009835f3
Erik Holmgren <erikholmgren24@gmail.com>
2021-11-29 15:00:21 +0100

    Add GlacierBed
    This adds the GlacierBed class, an object for storing information about
    the glacier bed. Initialisation can be done in two ways: Either with
    single scalars for top, bottom and width or with altitude/width pairs
    for greater control of the geometry. Also changes the glacier class to
    accept the bed for initalisaiton.
    
    Also contains some changes to the
    plotting.

--- oggm_edu/glacier.py
+++ oggm_edu/glacier.py
@@ -19,39 +19,176 @@ from oggm import cfg
 cfg.initialize_minimal()
 sns.set_context('notebook')
 sns.set_style('ticks')
-plt.rcParams['figure.figsize'] = (7, 5)
+plt.rcParams['figure.figsize'] = (10, 9)
 
 
-class Glacier:
-    '''The glacier object'''
+class GlacierBed:
+    '''The glacier bed'''
 
-    def __init__(self, top, bottom, width, nx=200, map_dx=100):
-        '''Initialise a glacier.
+    def __init__(self, top=None, bottom=None, width=None, altitudes=None,
+                 widths=None, nx=200, map_dx=100):
+        '''Initialise the bed. Pass single scalars for top, bottom and width
+         to create a square glacier bed. For finer control pass altitudes and
+         widths as lists or tuples for custom geometry. Will linearly
+         intepolate between the altitude/width pairs.
 
         Parameters
         ----------
-        top : float
-            Elevation at the top of the glacier domain in meters.
-        bottom : float
-            Elevation at the bottom of the glacier domain in meters.
-        widths : float
-            Width of the glacier in meters.
+        top : int/float
+            Elevation at the top of the bed domain in meters.
+        bottom : int/float
+            Elevation at the bottom of the bed domain in meters.
+        width : int/float
+            Width of the bed in meters. Generates a square bed.
+        altitudes : int/float, list/tuple
+            list of values corresponding to the altitude/width distribution.
+            First and last value will be the top and bottom.
+            Length should match widths.
+        widths : int/float, list/tuple
+            List of values defining the widths along the glacier.
+            Length should match altitudes.
         nx : int
             Number of grid points.
         map_dx : int
             Grid point spacing in meters.
         '''
+        # Arg checks
+        if (top is None and altitudes is None) or \
+                (bottom is None and altitudes is None) or \
+                (top is not None and altitudes is not None) or \
+                (bottom is not None and altitudes is not None):
+            raise ValueError('Provide either a top and bottom or altitudes.')
+
+        if (width is None and widths is None) or \
+                (width is not None and widths is not None):
+            raise ValueError('Provide either a single width'
+                             ' or a list of widths')
+        if (altitudes is not None and width is not None):
+            raise ValueError('Not possible to combine altitude with width.')
+        if (top is not None and bottom is not None and widths is not None):
+            raise ValueError('Not possible to combine widths '
+                             'with top and bottom.')
+
+        # Check that width is a single scalar.
+        if width is not None:
+            try:
+                width + 1
+            except TypeError:
+                raise TypeError('Width should be single scalar (int/float)')
 
         # Geometry
-        self.top = top
-        self.bottom = bottom
+        # If we have top and bottom.
+        if top and bottom:
+            self.top = top
+            self.bottom = bottom
+        # Else get it from the altitudes.
+        else:
+            self.top = altitudes[0]
+            self.bottom = altitudes[-1]
+
+        # Check the values of top and bottom.
+        if self.top <= self.bottom and self.bottom < 0:
+            raise ValueError('Top of the bed has to be above the bottom.' +
+                             ' Bottom also has to be above 0')
         self.map_dx = map_dx
-        self.width = width
-        self.widths = (np.zeros(nx) + self.width) / map_dx
         # Initialise the bed
-        self.bed_h = np.linspace(top, bottom, nx)
-        self.surface_h = self.bed_h
+        self.bed_h = np.linspace(self.top, self.bottom, nx)
         self.distance_along_glacier = np.linspace(0, nx, nx) * map_dx * 1e-3
+        # Widths.
+        # If width has a length of one, we have a constant width.
+        if width:
+            self.width = width
+            self.widths = (np.zeros(nx) + self.width) / map_dx
+        # If length of width and length of width altitudes are compatible,
+        # we create a bed with variable width.
+        elif len(altitudes) == len(widths):
+            self.width = widths
+            # First create a constant bed.
+            tmp_w = np.zeros(nx)
+
+            altitudes = list(altitudes)
+            widths = list(widths)
+            # Check that altitudes make sense.
+            altitudes_tmp = altitudes.copy()
+            altitudes_tmp.sort(reverse=True)
+            if not altitudes_tmp == altitudes:
+                raise ValueError('Please provides altitudes'
+                                 ' in descending order.')
+            # Loop over the altitude/width pairs and do a linear
+            # interpolations between them.
+            for i, alt in enumerate(altitudes[1:]):
+                # We want to interpolate to the previos altitude step.
+                inter_top = altitudes[i]
+                # Select the values that we interpolate.
+                mask = np.logical_and(self.bed_h <= inter_top,
+                                      self.bed_h >= alt)
+                # Linear interpolation between the widths.
+                tmp_w[mask] = np.linspace(self.width[i], self.width[i+1],
+                                          mask.sum())
+            # Assign the varied widths it.
+            self.widths = tmp_w / map_dx
+
+        # If noting works, raise an error.
+        else:
+            raise ValueError('Width and altitude widths are not compatible.')
+
+    def __repr__(self):
+        w_string = ('constant' if type(self.width) in (int, float)
+                    else 'variable')
+        string = f'Linear glacier bed with a {w_string} width.' \
+                 f'\n Top: {self.top} m.' \
+                 f'\n Bottom: {self.bottom} m.' \
+                 f'\n Widths: {self.width}'
+        return string
+
+    def plot(self):
+        '''Plot the glacier bed'''
+
+        fig, (ax1, ax2) = plt.subplots(nrows=2,
+                                       gridspec_kw={'height_ratios': [2, 1]},
+                                       sharex=True)
+
+        # Plot the bed
+        ax1.plot(self.distance_along_glacier, self.bed_h, label='Bedrock',
+                 ls=':', c='k', lw=2, zorder=3)
+        # And fill it.
+        ax1.fill_betweenx(self.bed_h, self.distance_along_glacier,
+                          color='lightgrey')
+        ax1.set_ylabel('Altitude [m]')
+        ax1.set_facecolor('#ADD8E6')
+        ax1.set_ylim(self.bottom, self.top + 400)
+        # Fill the bed.
+        ax2.fill_between(self.distance_along_glacier,
+                         -self.widths/2 * self.map_dx,
+                         self.widths/2 * self.map_dx,
+                         color='lightgrey')
+        ax2.set_facecolor('darkgrey')
+        ax2.axhline(0, c='k')
+        plt.xlim((0, self.distance_along_glacier[-1] + 2))
+        plt.xlabel('Distance along glacier [km]')
+        plt.ylabel('Distance from centerline [m]')
+        ax1.set_title('Glacier domain')
+        ax1.legend()
+
+
+class Glacier:
+    '''The glacier object'''
+
+    def __init__(self, bed):
+        '''Initialise a glacier.
+
+        Parameters
+        ----------
+        bed : bed object
+
+        '''
+        # Check the type of the bed.
+        if not isinstance(bed, GlacierBed):
+            raise TypeError('The bed has to be of the type GlacierBed.')
+        # Save the bed.
+        self.bed = bed
+        # Set the surface height.
+        self.surface_h = self.bed.bed_h
 
         # Mass balance
         self._ELA = None
@@ -73,18 +210,18 @@ class Glacier:
         # Do we have a current state?
         state = self.state()
         string = f'Glacier at year {int(self.age)}' \
-                 + f'\nTop eleveation {self.top} m.a.s.l.' \
-                 + f'\nBottom elevation {self.bottom} m.a.s.l' \
                  + f'\nELA: {self.ELA}' \
-                 + f'\nWidth {self.width} m.\nLength {state.length_m} m.' \
+                 + f'\nLength {state.length_m} m.' \
                  + f'\nArea {state.area_km2:.3f} km2.\nVolume' \
-                 + f' {state.volume_km3:.3f} km3'
+                 + f' {state.volume_km3:.3f} km3' \
+                 + f'\n{self.bed}'
         return string
 
     def init_flowline(self):
         return RectangularBedFlowline(surface_h=self.surface_h,
-                                      bed_h=self.bed_h,
-                                      widths=self.widths, map_dx=self.map_dx)
+                                      bed_h=self.bed.bed_h,
+                                      widths=self.bed.widths,
+                                      map_dx=self.bed.map_dx)
 
     @property
     def ELA(self):
@@ -221,35 +358,87 @@ class Glacier:
 
     def plot(self):
         '''Plot the glacier'''
-        # Bedrock
-        plt.plot(self.distance_along_glacier, self.bed_h, label='Bedrock',
-                 ls=':', c='k', lw=2, zorder=3)
-        plt.fill_betweenx(self.bed_h, self.distance_along_glacier,
-                          facecolor='grey', alpha=0.3)
-        # Plot the initial galcier surface
-        plt.plot(self.distance_along_glacier, self.initial_state.surface_h,
+        fig, (ax1, ax2) = plt.subplots(nrows=2,
+                                       gridspec_kw={'height_ratios': [2, 1]},
+                                       sharex=True)
+
+        # Plot the bed
+        ax1.plot(self.bed.distance_along_glacier, self.bed.bed_h,
+                 label='Bedrock', ls=':', c='k', lw=2, zorder=3)
+        # And fill it.
+        ax1.fill_betweenx(self.bed.bed_h, self.bed.distance_along_glacier,
+                          color='lightgrey')
+        ax1.set_ylabel('Altitude [m]')
+
+        # Plot the initial glacier surface
+        ax1.plot(self.bed.distance_along_glacier, self.initial_state.surface_h,
                  label='Initial glacier surface height', c='tab:orange')
+
+        # Set face color of side view.
+        ax1.set_facecolor('#ADD8E6')
+        ax1.set_ylim((self.bed.bottom, self.bed.top + 200))
+
+        # Top down stuff
+        # Fill the bed on the top down view.
+        ax2.fill_between(self.bed.distance_along_glacier,
+                         -self.bed.widths/2 * self.bed.map_dx,
+                         self.bed.widths/2 * self.bed.map_dx,
+                         color='lightgrey')
+        ax2.axhline(0, c='k', zorder=3)
+        ax2.set_facecolor('darkgrey')
+        ax2.set_ylabel('Distance from centerline [m]')
+
         # If we have a current state, plot it.
         if self.current_state is not None:
-            plt.plot(self.distance_along_glacier, self.current_state.surface_h,
-                     label='Current glacier surface height')
+            ax1.fill_between(self.bed.distance_along_glacier,
+                             self.bed.bed_h,
+                             self.current_state.surface_h,
+                             color='white',
+                             edgecolor='C0',
+                             lw=2,
+                             label='Current glacier surface height')
+            # Fill in the glacier in the topdown view.
+            # Where does the glacier have thickness?
+            filled = np.where(self.current_state.thick > 0, self.bed.widths, 0)
+            # Fill vetween them
+            ax2.fill_between(self.bed.distance_along_glacier,
+                             -filled/2 * self.bed.map_dx,
+                             filled/2 * self.bed.map_dx,
+                             where=filled > 0,
+                             color='white',
+                             edgecolor='C0',
+                             lw=2
+                             )
+            ax1.set_ylim((self.bed.bottom,
+                          self.current_state.surface_h[0] + 200))
 
         # ELA
         if self.ELA is not None:
-            plt.axhline(self.ELA, ls='--', c='k', lw=1)
-            plt.text(self.distance_along_glacier[-1], self.ELA + 10,
+            ax1.axhline(self.ELA, ls='--', c='k', lw=1)
+            ax1.text(self.bed.distance_along_glacier[-1], self.ELA + 10,
                      'ELA', horizontalalignment='right',
                      verticalalignment='bottom')
+            # Where along the bed is the ELA? Convert height to
+            # distance along glacier kind of.
+            idx = (np.abs(self.bed.bed_h - self.ELA)).argmin()
+            # Plot the ELA in top down
+            ax2.vlines(self.bed.distance_along_glacier[idx],
+                       ymin=-self.bed.widths[idx]/2 * self.bed.map_dx,
+                       ymax=self.bed.widths[idx]/2 * self.bed.map_dx,
+                       color='k', ls='--', lw=1)
+
+        # Limits etc
+        plt.xlim((0, self.bed.distance_along_glacier[-1] + 2))
 
         # Decorations
-        plt.xlabel('Distance along glacier [km]')
-        plt.ylabel('Altitude [m]')
-        plt.title(f'Glacier state at year {int(self.age)}')
-        plt.legend(loc='lower left')
+        ax1.set_title(f'Glacier state at year {int(self.age)}')
+        ax1.legend(loc='lower left')
+        ax2.set_xlabel('Distance along glacier [km]')
 
     def plot_mass_balance(self):
         '''Plot the mass balance profile of the glacier'''
-        plt.plot(self.annual_mass_balance(), self.bed_h, label='Mass balance',
+        plt.plot(self.annual_mass_balance(), self.bed.bed_h,
+                 label='Mass balance',
                  c='tab:orange')
         plt.xlabel('Annual mass balance [m yr-1]')
         plt.ylabel('Altitude [m]')
