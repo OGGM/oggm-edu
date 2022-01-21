8a32f56e50b6a6702d1d1d1acbe63797d4ea88d2
Erik Holmgren <erikholmgren24@gmail.com>
2022-01-21 09:25:54 +0100

    Adds a new namespace for the old edu functions, now found under legacy_functions. This will break previous usage of oggm_edu, hence anything relying on oggm_edu functions either need to update import statements or pin the version of oggm_edu.

--- oggm_edu/__init__.py
+++ oggm_edu/__init__.py
@@ -1,6 +1,6 @@
 __version__ = '0.0.1'
 
-from .funcs import *
+import oggm_edu.funcs as legacy_functions
 from oggm_edu.glacier import Glacier, SurgingGlacier
 from oggm_edu.glacierBed import GlacierBed
 from oggm_edu.glacierCollection import GlacierCollection
