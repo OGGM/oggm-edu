5cdd34052e67f87a2e694aae6241a88db0b8c799
Fabien Maussion <fabien.maussion@uibk.ac.at>
2026-05-15 16:11:13 +0100

    Update survey deadline in announcement message

--- docs/conf.py
+++ docs/conf.py
@@ -179,7 +179,7 @@ html_theme_options = {
     "announcement": (
         '<p>How do you use OGGM-Edu? Tell us by filling '
         '<a href="https://forms.office.com/e/ZDBCwj3vFb" target="_blank">this short survey</a> '
-        '(open until May 17). Thanks for your contribution!</p>'
+        '(open until May 20). Thanks for your contribution!</p>'
     ),
 }
 
